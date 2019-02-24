8c7d9ea70b600145f424f05b1200b797731cfd4f
Matt Finnell <mattfinnell104@gmail.com>
2019-02-24 03:08:58 -0800

    WIP

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
 import SiteNavBar from './SiteNavBar';
 import Home from "../pages/Home";
 import Projects from "../pages/Projects";
@@ -10,16 +11,16 @@ import About from "../pages/About";
 export default class App extends React.Component {
   render() {
     return (
-        <div>
-          <SiteNavBar/>
-          <div className="page">
-            <Route exact path="/" component={Home}/>
-            <Route path="/projects" component={Projects}/>
-            <Route path="/passions" component={Passions}/>
-            <Route path="/about" component={About}/>
-            <Route path="/contact" component={Contact}/>
-          </div>
-        </div>
+      <div>
+        <SiteNavBar/>
+        <section className="page">
+          <Route exact path="/" component={Home}/>
+          <Route path="/projects" component={Projects}/>
+          <Route path="/passions" component={Passions}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact}/>
+        </section>
+      </div>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+ 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faIgloo)
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Matt Finnell</h1>
-        <p>Seattle based Software Engineer</p>
-      </div>
+      <Container className="align-middle">
+        <Row><h1>Matt Finnell</h1></Row>
+        <Row><p>Seattle based Software Engineer</p></Row>
+        <Row>
+          <Col md={3}><LinkContainer to="/projects"><Button>Projects</Button></LinkContainer></Col>
+          <Col md={3}><LinkContainer to="/passions"><Button>Passions</Button></LinkContainer></Col>
+          <Col md={3}><LinkContainer to="/about"><Button>About</Button></LinkContainer></Col>
+          <Col md={3}><LinkContainer to="/contact"><Button>Contact</Button></LinkContainer></Col>
+        </Row>
+        <Row><FontAwesomeIcon icon="igloo" /></Row>
+      </Container>
     )
   }
 }
