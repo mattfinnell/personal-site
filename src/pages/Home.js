e5171422b01efa3a66216626dbfee05214ada52e
Matt Finnell <mattfinnell104@gmail.com>
2019-05-14 03:38:19 -0700

    touched up the bootstrap structure for the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col, Button} from 'react-bootstrap';
  
-export default class Home extends Component {
+export default class Home extends React.Component {
   render() {
     return (
-      <Container className="align-middle">
-        <Row><h1>Matt Finnell</h1></Row>
-        <Row><p>Seattle Based Software Engineer</p></Row>
+      <Container className="text-center">
+        <Row><Col md={12}><h1>Matt Finnell</h1></Col></Row>
+        <Row><Col md={12}><p>Seattle Based Software Engineer</p></Col></Row>
         <Row>
-          <Col md={4}><LinkContainer to="/projects"><Button>Projects</Button></LinkContainer></Col>
-          <Col md={4}><LinkContainer to="/about"><Button>About</Button></LinkContainer></Col>
-          <Col md={4}><LinkContainer to="/contact"><Button>Contact</Button></LinkContainer></Col>
+          <Col sm={{span: 2, offset: 3}}><LinkContainer to="/projects"><Button>Projects</Button></LinkContainer></Col>
+          <Col sm={2}><LinkContainer to="/about"><Button>About</Button></LinkContainer></Col>
+          <Col sm={2}><LinkContainer to="/contact"><Button>Contact</Button></LinkContainer></Col>
         </Row>
         <Row>
-          <a href="https://github.com/mattfinnell"><i className="fab fa-github"></i></a>
-          <a href="https://www.linkedin.com/in/mattfinnell/"><i className="fab fa-linkedin"></i></a>
+          <Col md={12}>
+            <h4>
+              <a href="https://github.com/mattfinnell"><i className="fab fa-github"></i></a>
+              <a href="https://www.linkedin.com/in/mattfinnell/"><i className="fab fa-linkedin"></i></a>
+            </h4>
+          </Col>
         </Row>
       </Container>
     )
